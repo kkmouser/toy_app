7dcd7084168a7158965e0e4c9ea3b0df09c01a02
kkmouser <karissa.mouser@my.southcentral.edu>
2015-11-05 15:19:24 +0000

    toy commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-class Micropost < ActiveRecord::Base
-  belongs_to :user
-  validates :content, length: { maximum: 140 }
+class User < ActiveRecord::Base
+  has_many :microposts
+  validates :name,  presence: true
+  validates :email, presence: true
 end
